d4089e0e5601ad4f6ee413ac3cb31013119ab789
Nureke-ALA02 <nurdaulet.nurkassym@narxoz.kz>
2026-09-08 17:13:12 +0500

    add new class account and add getter setter

---
+++ src/main/java/com/bank/model/account.java
@@ -0,0 +1,37 @@
+package com.bank.model;
+
+public class account {
+    private Long id ;
+    private User owner;
+    private double balance ;
+
+    public account(Long id, User owner, double balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
