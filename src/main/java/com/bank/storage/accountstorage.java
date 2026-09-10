989c4119538f8ed0e21087a7ed97495365431676
Nureke-ALA02 <nurdaulet.nurkassym@narxoz.kz>
2026-09-10 22:57:00 +0500

    class for save accounts

--- src/main/java/com/bank/storage/accountstorage.java
+++ src/main/java/com/bank/storage/accountstorage.java
@@ -1,4 +1,20 @@
 package com.bank.storage;
+import com.bank.model.account;
+import java.util.ArrayList;
+import java.util.List;
 
 public class accountstorage {
+    private List<account> accounts=new ArrayList<>();
+     public void add (account account){
+         accounts.add(account);
+     }
+
+     public account findByUserId(Long userId){
+         for  (account account:accounts){
+             if (account.getOwner().getId().equals(userId)){
+                 return account;
+             }
+         }
+         return null;
+     }
 }
