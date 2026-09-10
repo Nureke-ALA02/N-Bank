e1ce13ea7e3919b1e547d84b8e46ae24ca7974ff
Nureke-ALA02 <nurdaulet.nurkassym@narxoz.kz>
2026-09-10 22:06:34 +0500

    create new class for save data about user

--- src/main/java/com/bank/storage/userStorage.java
+++ src/main/java/com/bank/storage/userStorage.java
@@ -1,4 +1,32 @@
 package com.bank.storage;
+import com.bank.model.User;
+import java.util.ArrayList;
+import java.util.List;
 
 public class userStorage {
+    private List<User> users= new ArrayList<>();
+    public void save (User user){
+        users.add(user);
+
+    }
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findbyEmail(String email){
+        for (User user:users){
+            if (user.getEmail().equals(email)){
+                return user;
+            }
+        }
+        return null;
+    }
+    public User findById(Long id){
+        for (User user:users){
+            if (user.getId().equals(id)){
+                return user;
+            }
+        }
+        return null;
+    }
 }
