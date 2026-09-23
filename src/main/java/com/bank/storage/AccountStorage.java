fe013a908c3a595c36f093ea27f242898dfb883b
Nureke-ALA02 <nurdaulet.nurkassym@narxoz.kz>
2026-09-24 00:01:32 +0500

    update storage code

--- src/main/java/com/bank/storage/AccountStorage.java
+++ src/main/java/com/bank/storage/AccountStorage.java
@@ -1,10 +1,13 @@
 package com.bank.storage;
 import com.bank.model.Account;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class AccountStorage {
-    private List<Account> accounts=new ArrayList<>();
+    private final List<Account> accounts=new ArrayList<>();
      public void add (Account account){
          accounts.add(account);
      }
@@ -17,4 +20,12 @@ public class AccountStorage {
          }
          return null;
      }
+    public Account findById(Long id) {
+        for (Account account :accounts) {
+            if (account.getId().equals(id)) {
+                return account;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/bank/storage/UserStorage.java
+++ src/main/java/com/bank/storage/UserStorage.java
@@ -1,10 +1,13 @@
 package com.bank.storage;
 import com.bank.model.User;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class UserStorage {
-    private List<User> users= new ArrayList<>();
+    private final List<User> users= new ArrayList<>();
     public void add (User user){
         users.add(user);
 
@@ -16,6 +19,14 @@ public class UserStorage {
     public User findbyEmail(String email){
         for (User user:users){
             if (user.getEmail().equals(email)){
+                return user;
+            }
+        }
+        return null;
+    }
+    public User findByIin(String iin){
+        for (User user : users){
+            if(user.getIin().equals(iin)){
                 return user;
             }
         }
